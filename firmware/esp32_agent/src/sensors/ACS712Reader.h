324313099638344307b7ff3acf79e1548fa4ad9b
hoorkazmi05-dotcom <hoorkazmi05@gmail.com>
2026-08-07 00:38:10 +0500

    Update ACS712Reader.h

--- firmware/esp32_agent/src/sensors/ACS712Reader.h
+++ firmware/esp32_agent/src/sensors/ACS712Reader.h
@@ -4,13 +4,30 @@
 #include <Arduino.h>
 
 class ACS712Reader {
-public:
-    void init(int pin);
-    float readCurrent();
 private:
-    int analogPin;
-    float offsetV;
-    float sensitivity;
+    uint8_t voltagePin;
+    uint8_t currentPin;
+
+public:
+    ACS712Reader(uint8_t vPin = 34, uint8_t cPin = 35) : voltagePin(vPin), currentPin(cPin) {}
+
+    void begin() {
+        analogSetAttenuation(ADC_11db);
+    }
+
+    float readVoltage() {
+        int raw = analogRead(voltagePin);
+        return (raw / 4095.0) * 25.0; // Scaled to 0-25V
+    }
+
+    float readCurrent() {
+        int raw = analogRead(currentPin);
+        return (raw / 4095.0) * 5.0; // Scaled to 0-5A
+    }
+
+    float readPower() {
+        return readVoltage() * readCurrent();
+    }
 };
 
 #endif
