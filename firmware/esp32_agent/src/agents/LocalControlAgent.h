bec710780e6bb3da1d6d102f59f3f590e65cd194
hoorkazmi05-dotcom <hoorkazmi05@gmail.com>
2026-08-07 00:40:35 +0500

    Update LocalControlAgent.h

--- firmware/esp32_agent/src/agents/LocalControlAgent.h
+++ firmware/esp32_agent/src/agents/LocalControlAgent.h
@@ -3,63 +3,43 @@
 
 #include <Arduino.h>
 #include "../config.h"
-#include "GenerationAgent.h"
-#include "StorageAgent.h"
-#include "LoadAgent.h"
-#include "TradingAgent.h"
-#include "../optimization/ADMMCensored.h"
-#include "../optimization/ConsensusProtocol.h"
-#include "../sensors/INA219Reader.h"
-#include "../sensors/ACS712Reader.h"
-
-struct SystemState {
-    float P_gen;
-    float P_load;
-    float P_bat;
-    float P_grid;
-    float SoC;
-    float I_bus;
-    float V_bus;
-    float T_board;
-    String neighbors[3];
-    int neighborCount;
-};
-
-struct BidMsg {
-    String from;
-    float quantity;
-    float price;
-    bool isSell;
-};
-
-struct ConsensusMsg {
-    String from;
-    String to;
-    int iteration;
-    float P_ex;
-    float u;
-};
 
 class LocalControlAgent {
-public:
-    void initSensors();
-    SystemState readState();
-    void handleBid(const BidMsg& bid);
-    void handleConsensus(const ConsensusMsg& msg);
-    void runConsensusStep();
-    void runADMMCensoredStep();
 private:
-    GenerationAgent ga;
-    StorageAgent sa;
-    LoadAgent la;
-    TradingAgent ta;
-    ADMMCensored admm;
-    ConsensusProtocol consensus;
-    INA219Reader ina219;
-    ACS712Reader acs712;
-    SystemState currentState;
-    float P_ex_proposed[3];
-    float P_ex_agreed[3];
+    uint8_t exportPin;
+    uint8_t importPin;
+    uint8_t curtailmentPin;
+
+public:
+    LocalControlAgent(uint8_t expPin = EXPORT_RELAY_PIN, uint8_t impPin = IMPORT_RELAY_PIN, uint8_t curtPin = CURTAILMENT_LED)
+        : exportPin(expPin), importPin(impPin), curtailmentPin(curtPin) {}
+
+    void begin() {
+        pinMode(exportPin, OUTPUT);
+        pinMode(importPin, OUTPUT);
+        pinMode(curtailmentPin, OUTPUT);
+        setIdle();
+    }
+
+    void executeAction(const String& action, bool curtailmentMode = false) {
+        if (action == "EXPORT") {
+            digitalWrite(exportPin, HIGH);
+            digitalWrite(importPin, LOW);
+        } else if (action == "IMPORT") {
+            digitalWrite(exportPin, LOW);
+            digitalWrite(importPin, HIGH);
+        } else {
+            setIdle();
+        }
+
+        digitalWrite(curtailmentPin, curtailmentMode ? HIGH : LOW);
+    }
+
+    void setIdle() {
+        digitalWrite(exportPin, LOW);
+        digitalWrite(importPin, LOW);
+        digitalWrite(curtailmentPin, LOW);
+    }
 };
 
 #endif
