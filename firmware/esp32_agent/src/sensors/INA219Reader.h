dceb6ce3f098700cc317fd8d5b16548c2235a07b
Hoor Zehra <hoorkazmi05@gmail.com>
2026-09-06 22:32:57 +0500

    Update INA219Reader.h

--- firmware/esp32_agent/src/sensors/INA219Reader.h
+++ firmware/esp32_agent/src/sensors/INA219Reader.h
@@ -1,17 +1,38 @@
-#ifndef INA219_READER_H
-#define INA219_READER_H
+#ifndef INA226_READER_H
+#define INA226_READER_H
 
-#include <Arduino.h>
 #include <Wire.h>
+#include <INA226_WE.h>
+#include "config.h"
 
-class INA219Reader {
-public:
-    bool init();
-    float readCurrent();
-    float readVoltage();
-    float readPower();
+#define INA226_I2C_ADDR 0x40
+
+class INA226Reader {
 private:
-    bool initialized;
+    INA226_WE ina226;
+
+public:
+    INA226Reader() : ina226(INA226_I2C_ADDR) {}
+
+    void begin() {
+        Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+        if (!ina226.init()) {
+            Serial.println("Failed to initialize INA226 sensor!");
+        }
+        ina226.waitUntilConversionCompleted();
+    }
+
+    float readVoltage() {
+        return ina226.getBusVoltage_V();
+    }
+
+    float readCurrent() {
+        return ina226.getCurrent_mA() / 1000.0; // Convert mA to Amperes
+    }
+
+    float readPower() {
+        return ina226.getBusPower();
+    }
 };
 
 #endif
